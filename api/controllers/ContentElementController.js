decdcec1055bfdae1f45701f744ed6e9415dc76e
drosi94 <b.drosat@hotmail.com>
2016-03-22 19:06:28 +0200

    -Create the API structure vol1
    Bill

---
+++ api/controllers/ContentElementController.js
@@ -0,0 +1,11 @@
+/**
+ * ContentElementController
+ *
+ * @description :: Server-side logic for managing Contentelements
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
---
+++ api/controllers/ContentTypeController.js
@@ -0,0 +1,11 @@
+/**
+ * ContentTypeController
+ *
+ * @description :: Server-side logic for managing Contenttypes
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
